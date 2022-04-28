acfc18143afd6067abb3a65f31e369d620580f04
Drew Hudec <drew@basistheory.com>
2022-04-28 12:06:42 -0400

    feat: Adding product hunt badge (#16)

---
+++ components/shared/ProductHuntBadge.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from '@mui/material/Link';
+
+export const ProductHuntBadge = () => (
+  <Link
+    href="https://www.producthunt.com/posts/sendsecure-ly?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-sendsecure&#0045;ly"
+    target="_blank"
+  >
+    <img
+      alt="sendsecure&#0046;ly - Send&#0032;secrets&#0032;to&#0032;anyone&#0032;without&#0032;leaving&#0032;a&#0032;digital&#0032;footprint | Product Hunt"
+      height="54"
+      src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=344273&theme=light"
+      style={{
+        width: '250px',
+        height: '54px',
+      }}
+      width="250"
+    />
+  </Link>
+);

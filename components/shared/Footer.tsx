d080e3e85f11d1586d8ae9d8909adee53e80cdc0
Josue Leon Sarkis <jolesa97@gmail.com>
2022-04-28 12:59:48 -0600

    feat: adding tos and pp (#18)
    * feat: adding tos and pp
    
    * feat: fixing lint
    
    * feat: cleanup
    
    * feat: fixing lint
    
    * feat: creating footer link component to reuse
    
    * feat: creating footer link component to reuse
    
    * feat: removing unneeded component

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import { CheckoutGitHub } from '@/components/shared/CheckOutGitHub';
 import { ProductHuntBadge } from '@/components/shared/ProductHuntBadge';
 import { SecurityInfo } from '@/components/shared/SecurityInfo';
+import { FooterLink } from './FooterLink';
 
 export const Footer = () => (
   <Box
@@ -14,8 +14,20 @@ export const Footer = () => (
     textAlign="center"
   >
     <SecurityInfo />
-    <Box mb={4} mt={4}>
-      <CheckoutGitHub />
+    <Box 
+      mb={4} 
+      mt={4} 
+      sx={{
+        display: 'inline-flex', 
+        justifyContent: 'center', 
+        color: '#838383', 
+        alignItems: 'center'
+      }}>
+        <FooterLink i18nKey='github.link' linkUrl='https://github.com/Basis-Theory-Labs/send-securely'/>
+        <Box ml={1} mr={1}>{'·'}</Box>
+        <FooterLink i18nKey='links.termsOfService' linkUrl='https://basistheory.com/terms-of-service'/>
+        <Box ml={1} mr={1}>{'·'}</Box>
+        <FooterLink i18nKey='links.privacyPolicy' linkUrl='https://basistheory.com/privacy-policy'/>
     </Box>
     <ProductHuntBadge />
   </Box>

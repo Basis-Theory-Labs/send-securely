921e41e30147a31d16e5116629e13eaab2056bf6
Adam Letizia <43392371+koalaty-code@users.noreply.github.com>
2022-05-11 09:14:31 -0500

    chore(tests): add integration tests for API endpoints, handle not found token (#26)

--- pages/api/secrets/[id]/index.ts
+++ pages/api/secrets/[id]/index.ts
@@ -6,16 +6,20 @@ export default async (req, res) => {
 
   const { id } = req.query;
 
-  const token = await bt.tokens.retrieve(id);
-  const datePlusTTL = add(parseISO(token.createdAt), {
-    seconds: Number.parseInt(token.metadata.ttl),
-  });
+  try {
+    const token = await bt.tokens.retrieve(id);
+    const datePlusTTL = add(parseISO(token.createdAt), {
+      seconds: Number.parseInt(token.metadata.ttl),
+    });
 
-  await bt.tokens.delete(id);
+    await bt.tokens.delete(id);
 
-  if (getTime(datePlusTTL) > Date.now()) {
-    res.status(200).json({ data: token.data });
-  } else {
+    if (getTime(datePlusTTL) > Date.now()) {
+      res.status(200).json({ data: token.data });
+    } else {
+      res.status(404).json({});
+    }
+  } catch {
     res.status(404).json({});
   }
 };

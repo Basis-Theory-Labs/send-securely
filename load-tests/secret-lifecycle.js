79bab35a1d1d04dfeee28196f96f7160cb208b47
Drew Hudec <drew@basistheory.com>
2022-05-26 09:15:58 -0400

    feat: Adds load tests (#33)

---
+++ load-tests/secret-lifecycle.js
@@ -0,0 +1,36 @@
+import {Trend} from 'k6/metrics';
+import http from 'k6/http'
+
+export const options = {
+  vus: 5,
+  iterations: 50,
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    create_duration: ["p(90)<500"],
+    retrieve_duration: ["p(90)<1000"]
+  }
+};
+
+const createSecretRequest = {
+  data: "my secret",
+  ttl: 600
+};
+
+const createTrend = new Trend("create_duration");
+const retrieveTrend = new Trend("retrieve_duration");
+
+export default function main() {
+  const createResponse = http.post(
+    `${__ENV.API_URL}/api/secrets`,
+    JSON.stringify(createSecretRequest),
+    {headers: {"Content-Type": "application/json"}}
+  );
+  createTrend.add(createResponse.timings.duration);
+
+  const secretId = createResponse.json()['id'];
+
+  const retrieveResponse = http.get(
+    `${__ENV.API_URL}/api/secrets/${secretId}`
+  );
+  retrieveTrend.add(retrieveResponse.timings.duration);
+}
